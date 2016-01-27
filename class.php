22c3af8a6089e006271f1ce58507c6f5247da1fe
Jim Witte <jim@thunderingbison.com>
2016-01-26 21:54:05 -0600

    refined object model ($Game object and $Turn object). refactored errors as an array. Game events are now properties of $Turn

--- class.php
+++ class.php
@@ -1,20 +1,76 @@
 <?php
 
+class Turn {
+	//player inputs
+	public $acresBought = 0;
+	public $acresSold = 0;
+	public $grainFed = 0;
+	public $acresPlanted = 0;
+	
+	//player input errors
+	public $errors = [];
+	
+	//turn events
+	public $impeach = FALSE;
+	public $harvest = 0;
+	public $ratLoss = 0;
+	public $peopleStarved = 0;
+	public $plagueDeaths = 0;
+	public $immigration = 0;
+	
+	function __construct() {
+		// get player inputs from form if available
+		$this->acresBought = 100;
+		$this->acresSold = 0;
+		$this->grainFed = 4000;
+		$this->acresPlanted = 400;
+	}
+	
+}
+
 class Game {
-    public $year;
-    public $landValue;
-    public $acresOwned;
-    public $population;
-    public $grainStored;
-    public $totalStarved;
+    public $year = 0;
+    public $landValue = 0;
+    public $acresOwned = 0;
+    public $population = 0;
+    public $grainStored = 0;
+    public $totalStarved = 0;
+    
+    public $turn;
 
     function __construct() {
-        $this->year = $_SESSION['year'];
-        $this->landValue = $_SESSION['landValue'];
-        $this->acresOwned = $_SESSION['acresOwned'];
-        $this->population = $_SESSION['population'];
-        $this->grainStored = $_SESSION['grainStored'];
-        $this->totalStarved = $_SESSION['totalStarved'];
+    
+    	session_start();
+    	// session_unset(); // clear out session
+    	
+    	// create a new turn for the game
+        $this->turn = new Turn();
+    	
+    	// if prior game data available, create game object with game in progress
+		if(isset($_SESSION['year'])) {
+        	$this->year = $_SESSION['year'];
+        	$this->landValue = $_SESSION['landValue'];
+        	$this->acresOwned = $_SESSION['acresOwned'];
+        	$this->population = $_SESSION['population'];
+        	$this->grainStored = $_SESSION['grainStored'];
+        	$this->totalStarved = $_SESSION['totalStarved'];
+
+        	$this->advanceTurn($this->turn); // game in progress, advance turn
+        } else {
+        	// default values for new game 
+            $this->year = 1;
+        	$this->landValue = 19;
+        	$this->acresOwned = 1000;
+        	$this->population = 100;
+        	$this->grainStored = 2000;
+        	$this->totalStarved = 0;
+        	// do not advance game turn, we are at year 1
+        	$this->writeGameState();
+        	 
+        }
+        
+
+
     }
 
 	function getGameState($field) {
@@ -22,56 +78,81 @@ class Game {
 		return $status;
 	}
 	
-	function validateTurn($acresBought, $acresSold, $grainFed, $acresPlanted) {
-		$error = '';
-		if ($acresSold < 0 or $acresBought < 0 or $grainFed < 0 or $acresPlanted < 0) {$error=$error."Values must be greater than 0. <br />";}
-		$bushelsUsed = ($acresBought * $this->landValue) + $grainFed + (2 * $acresPlanted);
-		$landSaleProceeds = $acresSold * $this->landValue;
-		if ($bushelsUsed > ($this->grainStored + $landSaleProceeds)) {$error=$error."Impossible. Not enough grain! <br />";}
-		if ($acresSold > $this->acresOwned){$error=$error."O Great One, you only have $this->acresOwned acres. <br />";}		
-		if ($acresPlanted > 10 * $this->population) {$error=$error."Not enough people to plant $acresPlanted acres! <br />";}
-		if ($error != '') {return $error; } else {return TRUE; }
+	function setGameState($field,$value) {
+		$this->$field = $value;
 	}
 	
-	function advanceTurn($acresBought, $acresSold, $grainFed, $acresPlanted) {
-		$harvest = $acresPlanted * rand(1, 6);
-		$bushelsUsed = ($acresBought * $this->landValue) + $grainFed + (2 * $acresPlanted);
-		$ratLoss = intval(rand(0,1) * $this->grainStored / rand(7,10));
+	function validateTurn($turn) {
+		$errors = [];
+		if ($turn->acresSold < 0 or $turn->acresBought < 0 or $turn->grainFed < 0 or $turn->acresPlanted < 0) {$errors[]="Values must be greater than 0.";}
 		
-		$peopleFed = intval($grainFed / 20);
-		$peopleStarved = $this->population - $peopleFed;
-		if ($peopleStarved < 0) { $peopleStarved = 0; }
+		$landSaleProceeds = intval($turn->acresSold * $this->landValue);
+		$landPurchaseCost = intval($turn->acresBought * $this->landValue);
+		$bushelsPlanted = intval($turn->acresPlanted * 2);
+		$totalBushelsUsed = intval($bushelsPlanted + $landPurchaseCost + $turn->grainFed - $landSaleProceeds);
+		if ($totalBushelsUsed > $this->grainStored) {$errors[]="O Great One, you don't have enough grain! Your edict requires $totalBushelsUsed bushels.";}
+		if ($turn->acresSold > $this->acresOwned){$errors[]="O Great One, I am unable to sell as you request. You have $this->acresOwned acres to sell.";}		
+		$peopleRequired = intval($turn->acresPlanted * 10);
+		if ($peopleRequired >  $this->population) {$errors[]="O Great One, not enough people to plant as you request. $peopleRequired people are needed to plant $turn->acresPlanted acres.";}
+		$turn->errors = $errors;
+	}
+	
+	function advanceTurn($turn) {
+	
+		//validate game turn inputs
+		$turn->errors = $this->validateTurn($turn);
+	
+		// harvest is 1-6 bushels per acre
+		$turn->harvest = intval($turn->acresPlanted * rand(1,6)); 
 		
+		// 50/50 chance of losing 10%-14% of stored grain
+		$turn->ratLoss = intval(rand(0,1) * $this->grainStored / rand(7,10)); 
 		
+		// 20 bushels required to feed each person. Can't feed more people than current population
+		$peopleFed = intval(min(($turn->grainFed / 20),$this->population));
+		$turn->peopleStarved = intval($this->population - $peopleFed);
+
+		$landPurchaseCost = intval($turn->acresBought * $this->landValue);
+		$landSaleProceeds = intval($turn->acresSold * $this->landValue);
+		$bushelsPlanted = intval($turn->acresPlanted * 2);
+		$totalBushelsUsed = intval($bushelsPlanted + $landPurchaseCost + $turn->grainFed - $landSaleProceeds);
+		
+		//15% chance of plague, kills half the population that didn't starve first
 		if (rand(1, 20) < 4) {
-			//15% chance of plague, kills half the population
-			$plagueDeaths = intval($this->population / 2);
-        } else {$plagueDeaths = 0;}
-        
-    	$totalDeaths = $plagueDeaths + $peopleStarved;
-    	echo "total deaths: $totalDeaths\n";
+			$plagueDeaths = intval(($this->population - $turn->peopleStarved) / 2);
+        } else {
+        	$plagueDeaths = 0;
+        }
+		
+		$totalDeaths = $plagueDeaths + $turn->peopleStarved;
+		
+		//chance of immigration if nobody died
 		if ($totalDeaths < 1 ) {
             $immigration = intval((20 * $this->acresOwned + $this->grainStored) / (100 * $this->population) + 1);
         } else {
             $immigration = 0;
         }
-	
-	
-		$impeach = FALSE;
-		if ($peopleStarved > .45 * $this->population) { $impeach = TRUE; }
-		
-		$results = array('impeach'=>$impeach, 'harvest'=>$harvest, 'ratLoss'=>$ratLoss, 'peopleStarved'=>$peopleStarved, 'plagueDeaths'=>$plagueDeaths, 'immigration'=>$immigration);
-	
-		// update game state 
-		$this->population = $this->population - $totalDeaths;
-		$this->grainStored = intval($this->grainStored + $harvest - $ratLoss - ($acresSold * $this->landValue) + ($acresBought * $this->landValue));
+        
+        //impeachment if more than 45% of the people starve in a single Turn
+        if ($turn->peopleStarved > .45 * $this->population) {
+        	$impeach = TRUE;
+        } else {
+        	$impeach = FALSE;
+        }
+        
+        // update game state 
+		$this->population = intval($this->population - $totalDeaths + $immigration);
+		$this->grainStored = intval($this->grainStored - $totalBushelsUsed - $turn->ratLoss);
 		// $this->year++; //increment by 1
-		$this->landValue = rand(17, 23);
-		$this->acresOwned = $this->acresOwned + $acresBought - $acresSold;
-		$this->totalStarved = $this->totalStarved + $totalDeaths;
+		$this->landValue = rand(15, 25);
+		$this->acresOwned = $this->acresOwned + $turn->acresBought - $turn->acresSold;
+		$this->totalStarved = $this->totalStarved + $turn->peopleStarved;
 		
-		return $results;
+		$this->writeGameState(); // write game state to session
+
+        
 	}
+	
 	
 	function writeGameState () {
 		$_SESSION['year'] = $this->year;
@@ -82,22 +163,6 @@ class Game {
 		$_SESSION['totalStarved'] = $this->totalStarved;
 	}
 	
-	function resetGameState () {
-		$_SESSION['year'] = '1';
-		$_SESSION['landValue'] = '19';
-		$_SESSION['acresOwned'] = '1000';
-		$_SESSION['population'] = '100';
-		$_SESSION['grainStored'] = '2800';
-		$_SESSION['totalStarved'] = '0';
-		
-		$this->year = $_SESSION['year'];
-        $this->landValue = $_SESSION['landValue'];
-        $this->acresOwned = $_SESSION['acresOwned'];
-        $this->population = $_SESSION['population'];
-        $this->grainStored = $_SESSION['grainStored'];
-        $this->totalStarved = $_SESSION['totalStarved'];
-
-	}
 	
 	function statusReport($events) {
 		$status = "You are in year ".$this->year." of your 10 year reign. ";
@@ -121,21 +186,11 @@ class Game {
 	
 }
 
-session_start();
-if(!isset($_SESSION['year'])) {
-	$_SESSION['year'] = '1';
-	$_SESSION['landValue'] = '19';
-	$_SESSION['acresOwned'] = '1000';
-	$_SESSION['population'] = '100';
-	$_SESSION['grainStored'] = '2800';
-	$_SESSION['totalStarved'] = '0';
-}
+
 
 $game = new Game();
-if ($game->validateTurn(0,0,2000,400)) {
-	$turn = $game->advanceTurn(0,0,1000,100);
-	// echo $game->statusReport ($turn);
-	include 'status.inc';
-}
+var_dump($game);
+
+
 
 ?>
